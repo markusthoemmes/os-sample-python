74640a141298780d7eb402a4f10bcd0a218648fd
Vincent Demeester <vincent@sbr.pm>
2018-10-22 09:08:38 +0200

    change hello
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Openshift!"
+    return "Hello Knative & Openshift!\n"
 
 if __name__ == "__main__":
     application.run()
