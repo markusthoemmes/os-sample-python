1e788de1c3d63eb664f87d684fb19c2de032d713
Vincent Demeester <vincent@sbr.pm>
2018-11-13 09:00:05 +0100

    random sleep
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from random import randint
+from time import sleep
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    sleep(randint(1, 10))
     return "Hello Knative & Openshift!\n"
 
 if __name__ == "__main__":
